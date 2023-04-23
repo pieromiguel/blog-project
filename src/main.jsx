15237735a6dc979d94ef7f738ddd01272f4b995e
Reyes Rondon <reyes192119@gmail.com>
2023-04-23 17:04:31 -0400

    changes App to Home

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Home from './Home.jsx'
 import Header from './components/header/Header.jsx'
 import Footer from './components/footer/Footer.jsx'
 import './index.css'
@@ -8,7 +8,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
-    <App />
+    <Home />
     <Footer />
   </React.StrictMode>,
 )
