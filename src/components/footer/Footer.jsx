5dcfb68b160a05d1508e8cc0bb2ec7e33f27a369
Reyes Rondon <reyes192119@gmail.com>
2023-04-23 16:58:54 -0400

    fixed bugs className

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,23 +6,23 @@ function Footer() {
   return (
     <>
 
-      <footer class="fixed bottom-0 w-full p-4">
-        <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-          <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="https://flowbite.com" class="flex items-center">
-              <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Blog</span>
+      <footer className="fixed bottom-0 w-full p-4">
+        <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+          <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+            <a href="https://flowbite.com" className="flex items-center">
+              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Blog</span>
             </a>
-            <div class="flex items-center lg:order-2">
-              <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+            <div className="flex items-center lg:order-2">
+              <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                 <li>
-                  <a href="#" class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Example</a>
+                  <a href="#" className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Example</a>
                 </li>
               </ul>
             </div>
-            <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-              <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+            <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+              <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                 <li>
-                  <a href="#" class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Example</a>
+                  <a href="#" className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Example</a>
                 </li>
               </ul>
             </div>
