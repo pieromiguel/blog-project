aa6d809374395f999dff9389b892e05fe6abf283
Reyes Rondon <reyes192119@gmail.com>
2023-04-23 16:36:42 -0400

    header, footer, bodz first setup

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,10 +6,29 @@ function Footer() {
   return (
     <>
 
-      <div>
-        <h1 className="text-3xl font-bold underline">Footer</h1>
-      </div>
-
+      <footer class="fixed bottom-0 w-full p-4">
+        <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+          <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+            <a href="https://flowbite.com" class="flex items-center">
+              <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Blog</span>
+            </a>
+            <div class="flex items-center lg:order-2">
+              <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                <li>
+                  <a href="#" class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Example</a>
+                </li>
+              </ul>
+            </div>
+            <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+              <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                <li>
+                  <a href="#" class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Example</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </footer>
 
     </>
   )
